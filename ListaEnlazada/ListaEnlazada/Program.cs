6e9dedfc6e878605a4e2dfcdeb8cbd6cefb3cc46
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-04-06 21:57:43 -0300

    NodeLinkedList

---
+++ ListaEnlazada/ListaEnlazada/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaEnlazada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crea listas enlazadas*/
+
+            LinkedList<String> nodo = new LinkedList<string>();
+
+            ListaNodo list = new ListaNodo();
+
+            //list.listaVacia();
+            list.addNodo(1);
+            list.addNodo(3);
+            list.addNodo(2);
+            list.addNodo(4);
+            list.Listar();
+            //list.DeleteFirst();
+            Console.WriteLine("");
+            //list.Listar();
+            //Console.WriteLine("");
+            list.Sustituir(3,6);
+            Console.WriteLine(list.Size());
+            Console.WriteLine(list.Buscar(3));
+            /*list.deletePosicionNodo(1);*/
+            list.Listar();
+            //list.listaVacia();
+            //Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
